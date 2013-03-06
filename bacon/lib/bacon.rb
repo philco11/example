7737b92ee3a2c4797937535b2829eaa7f2eb2828
Philip Cortes <philip.cortes@gmail.com>
2013-03-05 23:48:11 -0800

    initial commit

---
+++ bacon/lib/bacon.rb
@@ -0,0 +1,8 @@
+class Bacon
+  def edible?
+    true 
+    end
+    
+    def expired!
+      self.expired = true
+end end
---
+++ bacon/spec/bacon_spec.rb
@@ -0,0 +1,10 @@
+require 'bacon'
+describe Bacon do
+  subject { Bacon.new }
+  its(:edible?) { should be_true }
+  it "expired!" do
+    bacon = Bacon.new
+    bacon.expired!
+    bacon.expired.should be_true
+    end 
+    end
